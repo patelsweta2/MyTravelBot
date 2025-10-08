ae0cd6a384c7b9b1250602de11bc1cfe2096615c
Sweta Patel <sweta0442@gmail.com>
2025-10-08 21:43:12 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return <div className="text-3xl font-bold underline">Hello</div>;
+}
+
+export default App;
